c801b15b129813dfb6eef8d0aca1941ce925174e
Abdo Najjar <raf7awe.overlord@gmail.com>
2019-10-01 19:15:10 +0300

    draw circle aninmation using c programming

---
+++ circleAnimation.c
@@ -0,0 +1,33 @@
+
+//including graphics lib
+#include <graphics.h>
+
+//length of time
+#define Times 1000
+
+//main method
+void  main() {
+    //define graphics init attr
+int gd = DETECT , gm ;
+
+//instaniate graph
+initgraph(&gd , &gm , "..\\bgi");
+
+    
+
+    //draw circle
+    for (size_t i = 0; i < Times; i++)
+    {
+        //clear from mintor buffer
+        // cleardevice();
+
+        //draw arc
+        arc(getmaxx()/2,getmaxy()/2,i,1+i,50);
+            
+        delay(50);
+    }
+    
+
+    //fetch screen
+    getch();
+}
